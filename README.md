fa10ba561f462c5bc8f6850fff71a97b7b1a61f1
Gary Tan <saba555279@gmail.com>
2018-12-12 09:36:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Paloalto Config Parser
+Parse paloalto config from xml to csv.
+
+Usage
+----
+     python paloalto-config-parser.py [-h] -f <configuration-file> -o <output-file>
+
---
+++ paloalto-config-parser.py
@@ -0,0 +1,46 @@
+import xml.etree.ElementTree as ET
+import argparse
+import csv
+
+parser = argparse.ArgumentParser(description='Parsing security rules from palauto.')
+parser.add_argument('-f', action='store',
+					metavar='<configuration-file>',
+                    help='path to configuration file',
+                    required=True)
+parser.add_argument('-o', action='store',
+					metavar='<output-file>',
+                    help='path to output file',
+                    required=True)
+
+args = parser.parse_args()
+
+CONFIGFILE = vars(args)['f']
+OUTPUTFILE = vars(args)['o']
+
+print('Parsing: {}'.format(CONFIGFILE))
+
+tree = ET.parse(CONFIGFILE)
+root = tree.getroot()
+
+outdata = open(OUTPUTFILE, 'w')
+csvwriter = csv.writer(outdata)
+csvwriter.writerow(['Name', 'to', 'from', 'source', 'destination', 'source-user', 'category', 'application', 'service', 'hip-profiles', 'action'])
+
+
+for entry in root.find('.//devices/entry/vsys/entry/rulebase/security/rules'):
+	out = []
+
+	# name
+	out.append(entry.attrib['name'])
+
+	# rule format 'to', 'from', 'source', 'destination', 'source-user', 'category', 'application', 'service', 'hip-profiles'
+	for i in ['to', 'from', 'source', 'destination', 'source-user', 'category', 'application', 'service', 'hip-profiles']:
+		temp = [t.text for t in entry.find(i).findall('member')]
+		out.append('\n'.join(temp))
+
+	#action
+	out.append(entry.find('action').text)
+
+	csvwriter.writerow(out)
+
+print("COMPLETE!")
